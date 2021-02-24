5d8b93a6ff58e12a76f56c1e508e7fae720f766c
MarcoPenne <marco.pennese.97@gmail.com>
2021-02-24 12:09:01 +0100

    discrete dynamic extension added

---
+++ single_car_dynamic/CarModel.py
@@ -0,0 +1,159 @@
+from acados_template import AcadosModel
+from casadi import *
+#from casadi import SX, vertcat, sin, cos, Function
+from Path import Path
+class CarModel:
+
+    def __init__(self, path, l1, l2, fixed_obstacles, dT, n_lap, gamma, h_cbf):
+        self.path = path
+        self.l1 = l1
+        self.l2 = l2
+        self.dT = dT
+        self.gamma = gamma
+        self.h_cbf = h_cbf
+        self.model = export_car_ode_model_with_discrete_rk4(path, l1, l2, fixed_obstacles, dT, n_lap, gamma, h_cbf)
+
+
+def export_car_ode_model(path, l1, l2, fixed_obstacles, dT, n_lap, gamma, h_cbf):
+
+    model_name = 'car_ode'
+
+    # load track parameters
+    pathlength = path.get_len()
+    s0 = np.arange(0., n_lap*pathlength, 0.05)
+    kapparef = np.zeros_like(s0)
+    for i, s in enumerate(s0):
+        kapparef[i] = path.get_k(s)
+    
+    # compute spline interpolations
+    kapparef_s = interpolant("kapparef_s", "bspline", [s0], kapparef)
+
+    # set up states & controls
+    l = MX.sym('l')
+    s = MX.sym('s')
+    theta_tilde = MX.sym('theta_tilde')
+    v1 = MX.sym('v1')
+    omega = MX.sym('omega')
+    x = vertcat(s, l, theta_tilde, v1, omega)
+
+    # controls
+    a1 = MX.sym('a1')
+    aw = MX.sym('aw')
+    u = vertcat(a1, aw)
+    
+    # xdot
+    l_dot      = MX.sym('l_dot')
+    s_dot = MX.sym('s_dot')
+    theta_tilde_dot = MX.sym('theta_tilde_dot')
+    v1_dot = MX.sym('v1_dot')
+    omega_dot = MX.sym('omega_dot')
+    xdot = vertcat(s_dot, l_dot, theta_tilde_dot, v1_dot, omega_dot)
+
+    # algebraic variables
+    z = vertcat([])
+
+    # parameters
+    s_obs1 = MX.sym('s_obs1')
+    l_obs1 = MX.sym('l_obs1')
+    theta_tilde_obs1 = MX.sym('theta_tilde_obs1')
+    v_obs1 = MX.sym('v_obs1')
+    s_obs2 = MX.sym('s_obs2')
+    l_obs2 = MX.sym('l_obs2')
+    theta_tilde_obs2 = MX.sym('theta_tilde_obs2')
+    v_obs2 = MX.sym('v_obs2')
+    p = vertcat(s_obs1, l_obs1, theta_tilde_obs1, v_obs1, s_obs2, l_obs2, theta_tilde_obs2, v_obs2)
+    
+    # dynamics
+    f_expl = vertcat((v1  * cos(theta_tilde))/(1 - kapparef_s(s)*l),
+                    v1 * sin(theta_tilde),
+                    omega - ((kapparef_s(s) *v1*cos(theta_tilde)/(1- kapparef_s(s)*l))),
+                    a1,
+                    aw
+                    )
+
+    f_impl = xdot - f_expl
+
+    model = AcadosModel()
+
+    model.f_impl_expr = f_impl
+    model.f_expl_expr = f_expl
+    model.x = x
+    model.xdot = xdot
+    model.u = u
+    model.z = z
+    model.p = p
+    model.name = model_name
+
+    obs=None
+    if fixed_obstacles is not None:
+        obs = fixed_obstacles
+        lap_vector = np.zeros_like(obs)
+        lap_vector[:, 0] = path.get_len()
+        tmp_obs = fixed_obstacles
+        obs = np.concatenate((obs, tmp_obs - lap_vector), axis=0)
+
+        for lap in range(1, n_lap):
+            lap_vector = np.zeros_like(fixed_obstacles)
+            lap_vector[:, 0] = lap*path.get_len()
+            tmp_obs = fixed_obstacles
+            obs = np.concatenate((obs, tmp_obs + lap_vector), axis=0)
+
+    x_t_plus_1 = x + f_expl*dT
+
+    for lap in range(0, n_lap):
+        h_t = ((s - (s_obs1+lap*path.get_len()) )**4/(l1)**4) + ((l-l_obs1)**4/(l2)**4) - h_cbf
+        h_t_plus_1 = ((x_t_plus_1[0] - (s_obs1+v_obs1*dT +lap*path.get_len() ) )**4/(l1)**4) + ((x_t_plus_1[1] - l_obs1)**4/(l2)**4) - h_cbf
+        if lap==0:
+            model.con_h_expr = vertcat(h_t_plus_1 - h_t + gamma * h_t)
+        else:
+            model.con_h_expr = vertcat(model.con_h_expr, h_t_plus_1 - h_t + gamma * h_t)
+
+        h_t = ((s - (s_obs2+lap*path.get_len()) )**4/(l1)**4) + ((l-l_obs2)**4/(l2)**4) - h_cbf
+        h_t_plus_1 = ((x_t_plus_1[0] - (s_obs2+v_obs2*dT +lap*path.get_len() ) )**4/(l1)**4) + ((x_t_plus_1[1] - l_obs2)**4/(l2)**4) - h_cbf
+        model.con_h_expr = vertcat(model.con_h_expr, h_t_plus_1 - h_t + gamma * h_t)
+
+    if obs is not None:
+        for o in range(obs.shape[0]):
+            h_t = ((s-obs[o, 0])**4/(l1)**4) + ((l-obs[o, 1])**4/(l2)**4) - h_cbf
+            h_t_plus_1 = ((x_t_plus_1[0] - obs[o, 0])**4/(l1)**4) + ((x_t_plus_1[1] - obs[o, 1])**4/(l2)**4) - h_cbf
+        
+            
+            if model.con_h_expr is None:
+                model.con_h_expr = vertcat(h_t_plus_1 - h_t + gamma * h_t)
+            else:
+                model.con_h_expr = vertcat(model.con_h_expr, h_t_plus_1 - h_t + gamma * h_t)
+
+    return model
+
+
+def export_car_ode_model_with_discrete_rk4(path, l1, l2, fixed_obstacles, dT, n_lap, gamma, h_cbf):
+
+    model = export_car_ode_model(path, l1, l2, fixed_obstacles, dT, n_lap, gamma, h_cbf)
+
+    x = model.x
+    u = model.u
+    nx = x.size()[0]
+
+    ode = Function('ode', [x, u], [model.f_expl_expr])
+    # set up RK4
+    k1 = ode(x,       u)
+    k2 = ode(x+dT/2*k1,u)
+    k3 = ode(x+dT/2*k2,u)
+    k4 = ode(x+dT*k3,  u)
+    xf = x + dT/6 * (k1 + 2*k2 + 2*k3 + k4)
+
+    model.disc_dyn_expr = xf
+    print("built RK4 for car model with dT = ", dT)
+    # print(xf)
+    # print()
+    # print(k1)
+    # print(k2)
+    # print(k3)
+    # print(k4)
+    
+    # print(xf.shape)
+    
+    #print( xf.set() )
+    
+    return model
+
---
+++ single_car_dynamic/utils.py
@@ -0,0 +1,195 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import datetime
+import os
+from tqdm import tqdm
+from Path import Path
+
+def transformProj2Orig(s, l, theta_tilde, path):
+    
+    X = np.zeros_like(s)
+    Y = np.zeros_like(s)
+    THETA = np.zeros_like(s)
+    for i in range(len(s)):
+        #print(s[i])
+        res = path(s[i])
+        #print(res)
+        (x1, y1) = res
+        theta_r = path.get_theta_r(s[i])
+        
+        x = x1 - np.sin(theta_r)*l[i]
+        y = y1 + np.cos(theta_r)*l[i]
+        
+        theta = theta_tilde[i] + theta_r
+        #print(x, y, theta)
+        X[i] = x
+        Y[i] = y
+        THETA[i] = theta
+    return (X, Y, THETA)
+
+def savePlot(x, y, theta, v, w, X_horizon, folder, i, car_model, fixed_obstacles, moving_obstacles, path, h_cbf):
+    plt.figure()
+    plt.xlim((-4, 14))
+    plt.ylim((-2, 12))
+    a = plt.gca()
+    a.set_aspect('equal')
+    #plt.plot(x, y, 'bo')
+
+    drawPath(path)
+    right_side = a.spines["right"]
+    right_side.set_visible(False)
+    #print(plt.axes().spines)
+    top_side = a.spines["top"]
+    top_side.set_visible(False)
+    h = car_model.l1
+    h2 = car_model.l2
+    half_edge = car_model.l2/2
+    t1 = plt.Polygon([[x+ (1/2)*h*np.cos(theta)- half_edge*np.sin(theta), y+(1/2)*h*np.sin(theta)+ half_edge*np.cos(theta)],  [x + half_edge*np.sin(theta)+(1/2)*h*np.cos(theta), y- half_edge*np.cos(theta)+(1/2)*h*np.sin(theta)], [x + half_edge*np.sin(theta)-(1/2)*h*np.cos(theta), y - half_edge*np.cos(theta)-(1/2)*h*np.sin(theta)]], color='blue')
+    plt.gca().add_patch(t1)
+    t1 = plt.Polygon([[x + half_edge*np.sin(theta)-(1/2)*h*np.cos(theta), y - half_edge*np.cos(theta)-(1/2)*h*np.sin(theta)], [x+ (1/2)*h*np.cos(theta)- half_edge*np.sin(theta), y+(1/2)*h*np.sin(theta)+ half_edge*np.cos(theta)], [x - half_edge*np.sin(theta)-(1/2)*h*np.cos(theta), y+ half_edge*np.cos(theta)-(1/2)*h*np.sin(theta)]], color='blue')
+    plt.gca().add_patch(t1)
+    t1 = t1 = plt.Polygon([[x+ (1/2)*h*np.cos(theta), y+ (1/2)*h*np.sin(theta)], [x - half_edge*np.sin(theta)-(1/2)*h*np.cos(theta), y+ half_edge*np.cos(theta)-(1/2)*h*np.sin(theta)], [x + half_edge*np.sin(theta)-(1/2)*h*np.cos(theta), y - half_edge*np.cos(theta)-(1/2)*h*np.sin(theta)]], color='red')
+    plt.gca().add_patch(t1)
+    
+    if fixed_obstacles is not None:
+        for o in range(fixed_obstacles.shape[0]):
+            obs = fixed_obstacles[o, :]
+            drawObstacles(obs, path, car_model, h_cbf)
+
+    for o in range(moving_obstacles.shape[0]):
+        obs = moving_obstacles[o, :3]
+        drawObstacles(obs, path, car_model, h_cbf)
+
+    #for i in range(X_horizon.shape[0]):
+    (_x, _y, _theta) = transformProj2Orig(X_horizon[:,0], X_horizon[:,1], X_horizon[:,2], path)
+    plt.plot(_x, _y, '-r', linewidth=0.5)
+    
+    # this is an inset axes over the main axes
+    a = plt.axes([.48, .78, .2, .2], facecolor='y')
+    a.set_ylabel('v')
+    plt.plot(v)
+    plt.ylim((-4, 4))
+    plt.xticks([])
+    #plt.yticks([])
+
+    a = plt.axes([.78, .78, .2, .2], facecolor='y')
+    a.set_ylabel('omega')
+    plt.plot(w)
+    plt.ylim((-2, 2))
+    plt.xticks([])
+    
+    plt.savefig('results/' + folder + "/%04d" % i +".png")
+
+    #plt.show()
+    plt.close()
+
+def drawObstacles(obs, path, car_model, h_cbf):
+    obs = transformProj2Orig([obs[0]], [obs[1]], [obs[2]], path)
+    h = car_model.l1
+    h2 = car_model.l2
+    half_edge = car_model.l2/2
+
+    x = obs[0][0]
+    y = obs[1][0]
+    theta = obs[2][0]
+    t2 = plt.Polygon([[x+ (1/2)*h*np.cos(theta)- half_edge*np.sin(theta), y+(1/2)*h*np.sin(theta)+ half_edge*np.cos(theta)],  [x + half_edge*np.sin(theta)+(1/2)*h*np.cos(theta), y- half_edge*np.cos(theta)+(1/2)*h*np.sin(theta)], [x + half_edge*np.sin(theta)-(1/2)*h*np.cos(theta), y - half_edge*np.cos(theta)-(1/2)*h*np.sin(theta)]], color='green')
+    plt.gca().add_patch(t2)
+    t2 = plt.Polygon([[x + half_edge*np.sin(theta)-(1/2)*h*np.cos(theta), y - half_edge*np.cos(theta)-(1/2)*h*np.sin(theta)], [x+ (1/2)*h*np.cos(theta)- half_edge*np.sin(theta), y+(1/2)*h*np.sin(theta)+ half_edge*np.cos(theta)], [x - half_edge*np.sin(theta)-(1/2)*h*np.cos(theta), y+ half_edge*np.cos(theta)-(1/2)*h*np.sin(theta)]], color='green')
+    plt.gca().add_patch(t2)
+    
+    delta = 0.025
+    xrange = np.arange(-4, 14, delta)
+    yrange = np.arange(-2, 12, delta)
+    X, Y = np.meshgrid(xrange,yrange)
+    
+    F = ((np.cos(-theta)*(X-x)-np.sin(-theta)*(Y-y))**4 / h**4 ) + ((np.sin(-theta)*(X-x)+np.cos(-theta)*(Y-y))**4 / h2**4)
+    plt.contour(X, Y, (F), [h_cbf], linestyles='dashed', linewidths=0.5, colors='blue')
+
+    
+def drawPath(path):
+    samples = np.arange(0., path.get_len(), 0.1)
+
+    coord = []
+    for s in samples:
+        coord += [path(s)]
+
+    x = [c[0] for c in coord]
+    y = [c[1] for c in coord]
+
+    plt.plot(x, y, '-y', linewidth=0.5)
+
+    inner_path = Path(path.l1, path.l2, path.r - 2)
+    samples = np.arange(0., inner_path.get_len(), 0.1)
+
+    coord = []
+    for s in samples:
+        coord += [inner_path(s)]
+
+    x = [c[0] for c in coord]
+    y = [c[1]+2 for c in coord]
+
+    plt.plot(x, y, '-k', linewidth=0.5)
+
+    ext_path = Path(path.l1, path.l2, path.r + 2)
+    samples = np.arange(0., ext_path.get_len(), 0.1)
+
+    coord = []
+    for s in samples:
+        coord += [ext_path(s)]
+
+    x = [c[0] for c in coord]
+    y = [c[1]-2 for c in coord]
+
+    plt.plot(x, y, '-k', linewidth=0.5)
+
+def renderVideo(simX, simU, simX_horizon, t, car_model, fixed_obstacles, simObs_position, path, folder, h_cbf):
+    # load track
+    s=simX[:,0]
+    l=simX[:,1]
+    theta_tilde=simX[:,2]
+
+    period = np.mean(np.diff(t))
+    fr = int(np.around(1/period, decimals=0))
+    
+    #print(s, l, theta_tilde)
+    # transform data
+    (x, y, theta) = transformProj2Orig(s, l, theta_tilde, path)
+    
+    # plot racetrack map
+    for i in tqdm(range(len(x)), desc="Creating frames of video"):
+        v = simU[:i, 0]
+        w = simU[:i, 1]
+        input_len = 40
+        if len(v)>input_len:
+            v = v[-input_len:]
+            w = w[-input_len:]
+
+        moving_obstacles = simObs_position[i, 0, :]
+        moving_obstacles = simObs_position[i]
+        moving_obstacles = moving_obstacles.reshape((2, 4))
+        savePlot(x[i], y[i], theta[i], v, w, simX_horizon[i, :, :],folder, i, car_model, fixed_obstacles, moving_obstacles, path, h_cbf)
+        #plt.show()
+    os.chdir('results/' + folder)
+    os.system(f"ffmpeg -framerate {fr}"+" -i %04d.png -r 30 -pix_fmt yuv420p video.mp4")
+    for i in tqdm(range(len(x)), desc="Removing temp files"):
+        os.system('rm %04d.png' %i)
+    os.chdir('../..')
+
+
+def plotRes(simX,simU,t):
+    # plot results
+    plt.figure()
+    plt.subplot(2, 1, 1)
+    plt.step(t, simU[:,0], color='r')
+    plt.step(t, simU[:,1], color='g')
+    plt.title('closed-loop simulation')
+    plt.legend(['a','aw'])
+    plt.ylabel('u')
+    plt.xlabel('t')
+    plt.grid(True)
+    plt.subplot(2, 1, 2)
+    plt.plot(t, simX[:,:])
+    plt.ylabel('x')
+    plt.xlabel('t')
+    plt.legend(['s','l','theta_tilde', 'v', 'w'])
+    plt.grid(True)
